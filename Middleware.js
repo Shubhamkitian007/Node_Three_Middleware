ff96b93a91ce60e971e1a57fa672b8ad92bb6c92
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-07-12 08:48:12 +0530

    Node Middleware

--- Middleware.js
+++ Middleware.js
@@ -3,33 +3,46 @@ const { response, request } = require("express");
 const express = require("express");
 const app = express();
 
+// First Middleware Create
+
 const FirstMiddleWare = (request, response, next)=>{
     console.log("First Middleware");
     next();
 }
+
+// Secong Middleware Create
+
 
 const SecondMiddleWare = (request, response, next)=>{
     console.log('Secong MiddleWare');
     next();
 }
 
+// Use First Middleware
 app.use(FirstMiddleWare);
+
+// Use Second Middleware
+
 app.use(SecondMiddleWare);
 
+// Call first Middleware
 app.get("/",FirstMiddleWare,(request, response)=>{
     response.send("First MiddleWare Send......")
 })
 
+// Call first Middleware
 app.get("/user", FirstMiddleWare, (request, response)=>{
     response.send("User Page First Middleware");
 })
 
+// Call first Middleware
 app.get("/student", FirstMiddleWare, (request, response)=>{
     response.send("Student Page First Middleware");
 })
 
+// Call Second Middleware
 app.get("/teacher", (request, response)=>{
     response.send("teacher Page Second Middleware");
 })
 
-app.listen(5550)
+app.listen(5550);
